2bcb96d66b0aab01f378216ab4f3528084e2ae89
Primael <primaelq@gmail.com>
2015-09-09 18:18:02 +0200

    Assets, Player controller, Scene
    Imported the base assets.
    Created a player controller with touch input.
    Made a test scene.
    Started a test world map scene.

---
+++ A-eye Studio Platformer/Assets/Scripts/GameMechanics/HideGUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideGUI : MonoBehaviour
+{
+    #if !UNITY_ANDROID && !UNITY_IPHONE && !UNITY_BLACKBERRY && !UNITY_WINRT || UNITY_EDITOR
+    void Start ()
+    {
+        Destroy(this.gameObject);
+	}
+    #endif
+}
---
+++ A-eye Studio Platformer/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 10.0f, jumpForce = 10.0f;
+    public LayerMask playerLayerMask;
+
+    Transform playerTransform, groundDetector;
+    Rigidbody2D playerRgb;
+
+    bool isGrounded = false;
+    float horizontalInput = 0.0f;
+
+	void Start ()
+	{
+        playerRgb = this.GetComponent<Rigidbody2D>();
+        playerTransform = this.transform;
+        groundDetector = GameObject.Find(this.name + "/GroundDetector").transform;
+	}
+
+	void Update ()
+	{
+        
+    }
+
+    void FixedUpdate ()
+    {
+        isGrounded = Physics2D.Linecast(playerTransform.position, groundDetector.position, playerLayerMask);
+
+        #if !UNITY_ANDROID && !UNITY_IPHONE && !UNITY_BLACKBERRY && !UNITY_WINRT
+
+        Move(Input.GetAxisRaw ("Horizontal"));
+
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+        
+        #else
+            Move(horizontalInput);
+        #endif
+
+    }
+
+    public void Move (float hInput)
+    {
+        Vector2 movementVel = playerRgb.velocity;
+        movementVel.x = hInput * speed;
+        playerRgb.velocity = movementVel;
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+            playerRgb.velocity += jumpForce * Vector2.up;
+    }
+
+    public void StartMoving (float hInput)
+    {
+        horizontalInput = hInput;
+    }
+}
